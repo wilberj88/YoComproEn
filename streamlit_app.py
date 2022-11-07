e3ba992721346b3567cc13ef0abc7a078aeea3c7
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-11-07 13:16:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,35 +25,35 @@ df = pd.read_csv(cargararchivo)
 st.dataframe(df)
 
 st.write("Desagregación de votos por barrios de acuerdo con las votaciones históricas")
-    #datos
-    df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-    st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
+#datos
+df = pd.DataFrame(
+np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+columns=['lat', 'lon'])
+st.pydeck_chart(pdk.Deck(
+map_style=None,
+initial_view_state=pdk.ViewState(
+    latitude=37.76,
+    longitude=-122.4,
+    zoom=11,
+    pitch=50,
+),
+layers=[
+    pdk.Layer(
+       'HexagonLayer',
+       data=df,
+       get_position='[lon, lat]',
+       radius=200,
+       elevation_scale=4,
+       elevation_range=[0, 1000],
+       pickable=True,
+       extruded=True,
     ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=df,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=df,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-        ],
-        ))
+    pdk.Layer(
+        'ScatterplotLayer',
+        data=df,
+        get_position='[lon, lat]',
+        get_color='[200, 30, 0, 160]',
+        get_radius=200,
+    ),
+    ],
+    ))
