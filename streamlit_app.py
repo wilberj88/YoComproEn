73a3159d8ffc1e6c5b81427a89735d489a842336
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-11-07 12:11:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
-import streamlit as st
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import altair as alt
+import pydeck as pdk
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Solutions", page_icon="🧠")
+
+st.title('Novus Solutions')
+st.header("Análisis de Datos de YoComproEn_MetroSt24")
+st.write("Ventas, Precios y Ubicaciones")
+
