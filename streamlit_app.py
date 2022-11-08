e7b4d7383b879a6fba7ea15aa5b37fc382c6ad5c
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-11-07 21:13:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,11 @@ if cargararchivo:
     st.write("Georeferenciación de las Ventas")
     #datos
     df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+    st.map(df)
+    
+    df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [43.47, 3.41],
     columns=['lat', 'lon'])
     st.pydeck_chart(pdk.Deck(
